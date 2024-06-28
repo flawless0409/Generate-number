d7e233df951fe367a76dd5ae4c3a2426b3f27ab6
flawless0409 <enehchinaecheremb0409@gmail.com>
2024-06-28 17:18:54 +0100

    first commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // randomNumber = Math.random().toString(16).substring(2, 8);
 // console.log(randomNumber);
 // It suppose to generate random colors,. but it's just the content that is changing when click...
-// So work onj the script......................
+// So work on the script......................
 const hex = document.querySelector(".hex");
 const btn = document.querySelector(".generate");
 const copy = document.querySelector(".copy");
@@ -12,7 +12,16 @@ const generateColor = function() {
     randomNumber = Math.random().toString(16).substring(2, 8);
     
     hex.value = "#" + randomNumber;
+    document.body.style.background = hex.value;
 }
 
-btn.addEventListener("click", generateColor)
+const copyColor = function(){
+    hex.select();
+    hex.setSelectionRange(0, 1000);
+    document.execCommand("click");
+    alert("Color has been copy to the clipboard")
+}
+
+btn.addEventListener("click", generateColor);
+copy.addEventListener("click", copyColor)
 // console.log (typeof randomNumber);
